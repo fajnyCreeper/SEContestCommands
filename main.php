6f944481d464872851e3d0d646bb1758b9638486
Creeperman007 <foltinmatej@gmail.com>
2020-06-10 01:14:30 +0200

    Set errors as plain text

--- main.php
+++ main.php
@@ -4,6 +4,7 @@ Credentials structure:
 $key = "random string";
 $bearer = "SE JWT token";
 */
+ini_set('html_errors', false);
 require_once("credentials.php");
 
 if (isset($_GET["key"], $_GET["args"]) && $_GET["key"] == $key)
