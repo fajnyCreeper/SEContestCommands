b57c30ddba92ab65397625ad3f0f0db10a8e32ca
Creeperman007 <foltinmatej@gmail.com>
2020-06-10 01:16:50 +0200

    No active contest
    Fixed issue, where API would return no active contest

--- active.php
+++ active.php
@@ -3,7 +3,7 @@ function GetActiveId($bot)
 {
   $res = $bot->contestsList();
 
-  if (is_null($res))
+  if (is_null($res) || is_null($res["active"]))
     return false;
     
   $contestId = $res["active"]["_id"];
